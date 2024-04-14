ebcf0607a63a23514def16ef361a06deb5256388
Matias Scherer Lunkes <mslunkes@outlook.com>
2024-04-14 18:38:46 -0300

    Finished layout and calculate

--- app/src/main/java/com/example/appgorjeta/MainActivity.kt
+++ app/src/main/java/com/example/appgorjeta/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.appgorjeta
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,12 +9,10 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.appgorjeta.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
-import java.util.IllegalFormatConversionException
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,52 +20,52 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         var percentage: Int = 0
-        binding.rbOptionOne.setOnCheckedChangeListener {_, isChecked ->
-            if (isChecked){
+        binding.rbOptionOne.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
                 percentage = 10
             }
         }
-        binding.rbOptionTwo.setOnCheckedChangeListener {_, isChecked ->
-            if (isChecked){
+        binding.rbOptionTwo.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
                 percentage = 15
             }
         }
-        binding.rbOptionThree.setOnCheckedChangeListener {_, isChecked ->
-            if (isChecked){
+        binding.rbOptionThree.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
                 percentage = 20
             }
         }
-                binding.btnClean.setOnClickListener(){
 
-        }
-
-        binding.btnCaculator.setOnClickListener {
+        binding.btnCaculator.setOnClickListener() {
             val totalTableTemp = binding.tieTotal.text
             val nPeopleTemp = binding.tieNumPeople.text
 
-            if (totalTableTemp?.isEmpty() == true || nPeopleTemp?.isEmpty() == true) {
-                Snackbar.make(binding.tieTotal, "Preencha todos os campos", Snackbar.LENGTH_LONG)
+            if (totalTableTemp?.isEmpty() == true ||
+                nPeopleTemp?.isEmpty() == true
+            ) {
+                Snackbar
+                    .make(binding.tieTotal, "Preencha todos os campos", Snackbar.LENGTH_LONG)
                     .show()
             } else {
                 val totalTable: Float = totalTableTemp.toString().toFloat()
                 val nPeople: Int = nPeopleTemp.toString().toInt()
 
                 val totalTemp = totalTable / nPeople
-                val Tips = percentage / 100
-                val totalWithTips = totalTemp + Tips
+                val tips = totalTemp * percentage / 100
+                val totalWithTips = totalTemp + tips
                 binding.tvResult.text = "Total com gorjeta: $totalWithTips"
 
             }
-
-            binding.btnClean.setOnClickListener() {
-                binding.tvResult.text = ""
-                binding.tieTotal.setText("")
-                binding.tieNumPeople.setText("")
-                binding.rbOptionOne.isChecked = false
-                binding.rbOptionTwo.isChecked = false
-                binding.rbOptionThree.isChecked = false
-            }
-
         }
+
+        binding.btnClean.setOnClickListener() {
+            binding.tvResult.text = ""
+            binding.tieTotal.setText("")
+            binding.tieNumPeople.setText("")
+            binding.rbOptionOne.isChecked = false
+            binding.rbOptionTwo.isChecked = false
+            binding.rbOptionThree.isChecked = false
+        }
+
     }
 }
